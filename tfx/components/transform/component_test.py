9cc2f7714cf216f7630acf998eef47ed092b5fff
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-07-17 17:50:40 -0700

    Added custom config to Transform component and made it available to pre-processing fn.
    PiperOrigin-RevId: 321888719

--- tfx/components/transform/component_test.py
+++ tfx/components/transform/component_test.py
@@ -76,6 +76,21 @@ class ComponentTest(tf.test.TestCase):
     self.assertEqual(preprocessing_fn,
                      transform.spec.exec_properties['preprocessing_fn'])
 
+  def testConstructFromPreprocessingFnWithCustomConfig(self):
+    preprocessing_fn = 'path.to.my_preprocessing_fn'
+    custom_config = {'param': 1}
+    transform = component.Transform(
+        examples=self.examples,
+        schema=self.schema,
+        preprocessing_fn=preprocessing_fn,
+        custom_config=custom_config,
+    )
+    self._verify_outputs(transform)
+    self.assertEqual(preprocessing_fn,
+                     transform.spec.exec_properties['preprocessing_fn'])
+    self.assertEqual(custom_config,
+                     transform.spec.exec_properties['custom_config'])
+
   def testConstructMissingUserModule(self):
     with self.assertRaises(ValueError):
       _ = component.Transform(
